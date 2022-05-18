123d1e866c665ceb82220b309036460df80554cf
maltamimi96 <46971446+maltamimi96@users.noreply.github.com>
2022-05-18 19:36:38 +1000

    Sign-in works
    ah ye

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,22 @@ class UsersController < ApplicationController
 def create
     @user=User.create(user_params)
     if @user.save
-        render json:@user,status: :created
+        auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+
+        render json:{username:@user.username,jwt: auth_token.token},status: :created
     else
         render json:@user.errors,status: :unprocessable_entity
     end        
+end
+
+def sign_in
+    @user=User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+        auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+        render json:{username:@user.username,jwt: auth_token.token},status: 200
+    else
+        render json: {error: "Incorrect Username or Password"},status: 400
+    end 
 end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   scope '/auth' do
     post'/sign_up',to: 'users#create'
+    post'/sign_in',to: 'users#sign_in'
+
   end
 end
