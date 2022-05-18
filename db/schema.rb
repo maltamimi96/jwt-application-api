fbf22ff77284e679322ac6c671f354d8ac323d5f
maltamimi96 <46971446+maltamimi96@users.noreply.github.com>
2022-05-18 23:23:50 +1000

    Creates Connection to users for questions
    Users are associated with questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_18_095818) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_131845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_095818) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["category_id"], name: "index_questions_on_category_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_095818) do
   end
 
   add_foreign_key "questions", "categories"
+  add_foreign_key "questions", "users"
 end
